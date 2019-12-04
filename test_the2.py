8abf71cffedc111464f183c60502580109f1a5f0
Foxy-Boxes <58276089+Foxy-Boxes@users.noreply.github.com>
2019-12-04 19:37:00 +0300

    Added files now you can see your faults

--- test_the2.py
+++ test_the2.py
@@ -3,11 +3,18 @@ from the2 import isCovered
 
 class TestCovered(unittest.TestCase):
     def test_isCovered(self):
-        with open("output.txt","r") as file:
-            data = file.read().split("\n")
-            for line in data:
-                if line != "":
-                    eval(line)
+        fails =[]
+        testfails= open("fails.txt",'a')
+        with open("proper.txt",'r') as file:
+            data = file.read()
+            exec(data)
             file.close
+        with open("output.txt",'r') as file:
+            testlist = file.read().split("\n")
+            for i in fails:
+                testfails.write(testlist[i]+"\n")
+            file.close
+        print("failed " + str(len(fails))+ " times")
+        print("fails can be found at fails.txt")
 if __name__ =="__main__":
     unittest.main()
